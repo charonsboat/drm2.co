b7aabd2a5b8015452b19cd3fcabbe5f9c74c0681
drmyersii <davidrmyersii@gmail.com>
2016-02-07 04:57:13 -0500

    Adding seeder for the blog_articles table.

---
+++ database/seeds/Blog_ArticleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Blog\Article;
+
+class Blog_ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $article = new Article();
+        $article->save();
+    }
+}

2158678b6d20325e7b746a4f80574df18f2c8ad2
drmyersii <davidrmyersii@gmail.com>
2016-02-07 04:59:10 -0500

    Adding public GetArticle method.

--- app/Services/Blog/BlogService.php
+++ app/Services/Blog/BlogService.php
@@ -2,8 +2,14 @@
 
 namespace App\Services\Blog;
 
+use App\Models\Blog\Article;
 
 class BlogService
 {
-    
+    public static function GetArticle($id)
+    {
+        $article = Article::find($id);
+
+        return $article;
+    }
 }

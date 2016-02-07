7d442be2c3781b45b00bedcec69bb66e1325a252
drmyersii <davidrmyersii@gmail.com>
2016-02-07 05:00:53 -0500

    Adding public GET method for retrieving an Article.

--- app/Http/Controllers/Blog/ArticleController.php
+++ app/Http/Controllers/Blog/ArticleController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\Blog\BaseController; // make sure we're using the correct BaseController
+use App\Services\Blog\BlogService;
 
 class ArticleController extends BaseController
 {
-    //
+    // GET /blog/articles/{id}
+    // Returns: Article
+    public function getArticle($id)
+    {
+        $article = BlogService::GetArticle($id);
+
+        // convert article to json data for transmission
+        return response()->json($article);
+    }
 }

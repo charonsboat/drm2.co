1279a6d1ace0c47a27300b20c0dfd6f4c1204254
drmyersii <davidrmyersii@gmail.com>
2016-02-07 02:32:56 -0500

    Adding base controller and article/user controller templates.

---
+++ app/Http/Controllers/Blog/ArticleController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\Blog\BaseController; // make sure we're using the correct BaseController
+
+class ArticleController extends BaseController
+{
+    //
+}
---
+++ app/Http/Controllers/Blog/BaseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\Controller; // we need to extend the Lumen base controller
+
+class BaseController extends Controller
+{
+    //
+}

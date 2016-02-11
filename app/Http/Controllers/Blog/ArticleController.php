f583a5f9e85a36e0f33d5e3d1a3c97c0f40557fa
drmyersii <davidrmyersii@gmail.com>
2016-02-11 00:05:48 -0500

    Adding createArticle controller method.

--- app/Http/Controllers/Blog/ArticleController.php
+++ app/Http/Controllers/Blog/ArticleController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\Blog\BaseController; // make sure we're using the correct BaseController
 use App\Services\Blog\BlogService;
+use Illuminate\Http\Request;
 
 class ArticleController extends BaseController
 {
+    // POST /blog/articles
+    // Returns: void
+    public function createArticle(Request $request)
+    {
+        $data = $request->json();
+
+        BlogService::CreateArticle($data->get('title'), $data->get('content'));
+    }
+
     // GET /blog/articles/{id}
     // Returns: Article
     public function getArticle($id)

0994a090d111c7ebda94b680833ff05067abdd23
drmyersii <davidrmyersii@gmail.com>
2016-02-11 00:04:57 -0500

    Adding method to BlogService to create new articles.

--- app/Services/Blog/BlogService.php
+++ app/Services/Blog/BlogService.php
@@ -3,13 +3,39 @@
 namespace App\Services\Blog;
 
 use App\Models\Blog\Article;
+use Parsedown;
 
 class BlogService
 {
+    /**
+     * Returns the Article with the specified identifier.
+     *
+     * @param $id string
+     * @return Article
+     */
     public static function GetArticle($id)
     {
         $article = Article::find($id);
 
         return $article;
+    }
+
+    /**
+     * Creates a new Article.
+     *
+     * @param $title string
+     * @param $content string
+     * @return void
+     */
+    public static function CreateArticle($title, $content)
+    {
+        $article = new Article();
+
+        $article->title   = $title;
+        $article->content = Parsedown::instance()
+                            ->setMarkupEscaped(true)
+                            ->text($content);
+
+        $article->save();
     }
 }

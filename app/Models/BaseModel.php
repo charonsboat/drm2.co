5a5240f5b27b9ac2ab7fdca55b1a6f6b836282b0
drmyersii <davidrmyersii@gmail.com>
2016-02-19 01:27:18 -0500

    Enabling soft deletes in the BaseModel.

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class BaseModel extends Model
 {
     use \App\Models\Traits\UuidKey;
+    use \Illuminate\Database\Eloquent\SoftDeletes;
 
     // disable auto-incrementing key generation for models
     public $incrementing = false;
+
+    // make sure dates mutated into Carbon objects
+    protected $dates = [ 'created_at', 'updated_at', 'deleted_at' ];
 }
